4648ae6b2e4b335dbc772e2fb767c10bdd510bd1
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-11-26 17:36:42 +0000

    a

--- ec2-main.tf
+++ ec2-main.tf
@@ -1,23 +1,22 @@
 module "ec2_instance" {
   source            = "./modules/ec2_module"
-  #region            = "us-east-1"
-  #ami               = "ami-12345678"
+  #region           = "us-east-1"
+  #ami              = "ami-12345678"
   instance_type     = "t2.micro"
   name              = "slb001"
-  
-  key_name          = "my-key"
-  root_volume_size  = 30
-  root_volume_type  = "gp3"
+  #key_name         = "my-key"
+  root_volume_size  = 20
+  root_volume_type  = "gp2"
   tags              = { 
     Env       = "dev"
-    Team      = "Infra"
+    Team      = "infra"
     Terraform = "yes" 
   }
 
   additional_volumes = [
     {
       device_name = "/dev/sdf"
-      size        = 50
+      size        = 10
       type        = "gp2"
     }
   ]
