89cb38de93a4a23924d0b9e002329be80219e9ee
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-11-26 17:09:46 +0000

    Create variables.tf

---
+++ modules/ec2_module/variables.tf
@@ -0,0 +1,52 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ami" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+}
+
+variable "tags" {
+  description = "Additional tags for the EC2 instance"
+  type        = map(string)
+  default     = {}
+}
+
+variable "key_name" {
+  description = "Key name to use for the EC2 instance"
+  type        = string
+}
+
+variable "root_volume_size" {
+  description = "Root volume size in GB"
+  type        = number
+}
+
+variable "root_volume_type" {
+  description = "Root volume type (e.g., gp2, gp3)"
+  type        = string
+}
+
+variable "additional_volumes" {
+  description = "Additional EBS volumes to attach"
+  type = list(object({
+    device_name = string
+    size        = number
+    type        = string
+  }))
+  default = []
+}
