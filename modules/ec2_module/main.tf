7f1c85dd4487d4656c00f311fb3dd7dce274d6db
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-11-26 17:09:31 +0000

    Create main.tf

---
+++ modules/ec2_module/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "this" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = merge(
+    {
+      "Name" = var.name
+    },
+    var.tags
+  )
+
+  root_block_device {
+    volume_size = var.root_volume_size
+    volume_type = var.root_volume_type
+  }
+
+  dynamic "ebs_block_device" {
+    for_each = var.additional_volumes
+    content {
+      device_name = ebs_block_device.value.device_name
+      volume_size = ebs_block_device.value.size
+      volume_type = ebs_block_device.value.type
+    }
+  }
+}
