03ac93212fcd499742b2289b52f4fb7f7e0e69ee
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-11-27 13:56:13 +0000

    a

--- variables.tf
+++ variables.tf
@@ -21,14 +21,6 @@ variable "AMIS" {
     }
 }
 
-variable "PATH_TO_PRIVATE_KEY" {
-  default = "levelup_key"
-}
-
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "levelup_key.pub"
-}
-
 variable "INSTANCE_USERNAME" {
   default = "ubuntu"
 }
