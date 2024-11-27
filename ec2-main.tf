05b7ba6d3f4f4296827e71c1af7df1a4115c546b
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-11-27 14:50:26 +0000

    a

--- ec2-main.tf
+++ ec2-main.tf
@@ -13,7 +13,7 @@ module "ec2_instance" {
   #availability_zone           = "eu-west-2a"
   create_additional_volume     = true
   additional_volume_name      = "slb001-volume001"
-  additional_volume_size      = 10
+  additional_volume_size      = 15
   #additional_volume_type      = "gp2"
   #additional_volume_device_name = "/dev/xdfh"
   tags              = { 
