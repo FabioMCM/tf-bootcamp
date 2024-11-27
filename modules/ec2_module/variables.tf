4500da347724cbcb89b80345ea4c8c6b825469a7
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-11-27 14:04:30 +0000

    a

--- modules/ec2_module/variables.tf
+++ modules/ec2_module/variables.tf
@@ -166,13 +166,13 @@ variable "availability_zone" {
 variable "additional_volume_name" {
   description = "Name tag for the additional volume"
   type        = string
-  default     = "null"
+  default     = null
 }
 
 variable "additional_volume_size" {
   description = "Size of the additional volume in GB"
-  type        = number
-  default     = "null"
+  type        = string
+  default     = null
 }
 
 variable "additional_volume_type" {
