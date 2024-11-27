39d30d2f9c336e3d98d5079b19dca3ce0dca12d8
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-11-27 14:11:59 +0000

    a

--- modules/ec2_module/variables.tf
+++ modules/ec2_module/variables.tf
@@ -172,7 +172,7 @@ variable "additional_volume_name" {
 variable "additional_volume_size" {
   description = "Size of the additional volume in GB"
   type        = number
-  default     = null
+  default     = 10
 }
 
 variable "additional_volume_type" {
