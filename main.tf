aac2799d0004aa6c10a789c62659d16164ef8321
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-12-03 12:25:59 +0000

    new pvc prod

--- main.tf
+++ main.tf
@@ -67,3 +67,18 @@ module "vpc" {
     "eu-west-2b" = "10.0.102.0/24"
   }
 }
+
+
+module "vpc" {
+  source         = "./modules/vpc_module"
+  name           = "prod-pvc"
+  vpc_cidr       = "10.0.0.0/16"
+  public_subnets = {
+    "eu-west-2a" = "10.0.3.0/24"
+    "eu-west-2b" = "10.0.4.0/24"
+  }
+  private_subnets = {
+    "eu-west-2a" = "10.0.103.0/24"
+    "eu-west-2b" = "10.0.104.0/24"
+  }
+}
