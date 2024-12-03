baf668f309c689985af548384472e7b6922cc7e6
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-12-03 12:35:37 +0000

    code adjustments

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 ###########################
 ###    EC2 INSTANCES    ###
 ###########################
-module "ec2_instance" {
+module "ec2_instance_slb001" {
   source                      = "./modules/ec2_module"
   #ami_id                      = "ami-0e8d228ad90af673b"
   #instance_type               = "t2.micro"
   #key_name                    = "ec2-ansible"
   instance_name               = "slb001"
-  subnet_id                   = "subnet-0082cb4d471785dd3"
+  #subnet_id                   = "subnet-0082cb4d471785dd3"
   associate_public_ip_address = true
   root_volume_size            = 20
   #root_volume_type            = "gp2"
@@ -24,19 +24,19 @@ module "ec2_instance" {
   }
 }
 
-module "ec2_instance_2" {
+module "ec2_instance_slb002" {
   source                      = "./modules/ec2_module"
   #ami_id                      = "ami-0e8d228ad90af673b"
   #instance_type               = "t2.micro"
   #key_name                    = "ec2-ansible"
-  instance_name               = "slb001"
-  subnet_id                   = "subnet-0082cb4d471785dd3"
+  instance_name               = "slb002"
+  #subnet_id                   = "subnet-0082cb4d471785dd3"
   associate_public_ip_address = true
   root_volume_size            = 20
   #root_volume_type            = "gp2"
   #availability_zone           = "eu-west-2a"
   create_additional_volume     = true
-  additional_volume_name      = "slb001-volume001"
+  additional_volume_name      = "slb002-volume001"
   additional_volume_size      = 20
   #additional_volume_type      = "gp2"
   #additional_volume_device_name = "/dev/xdfh"
@@ -76,7 +76,7 @@ module "ec2_instance_2" {
 ###########################
 ###    NETWORK - VPC    ###
 ###########################
-module "vpc" {
+module "vpc_dev" {
   source         = "./modules/vpc_module"
   name           = "dev-pvc"
   vpc_cidr       = "10.0.0.0/16"
@@ -87,5 +87,19 @@ module "vpc" {
   private_subnets = {
     "eu-west-2a" = "10.0.101.0/24"
     "eu-west-2b" = "10.0.102.0/24"
+  }
+}
+
+module "vpc_prod" {
+  source         = "./modules/vpc_module"
+  name           = "prod-pvc"
+  vpc_cidr       = "10.0.0.0/16"
+  public_subnets = {
+    "eu-west-2a" = "10.0.3.0/24"
+    "eu-west-2b" = "10.0.4.0/24"
+  }
+  private_subnets = {
+    "eu-west-2a" = "10.0.103.0/24"
+    "eu-west-2b" = "10.0.104.0/24"
   }
 }
