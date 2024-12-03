40cb8fbbebd043b85ed14b4001ac7f171f1f5f4c
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-12-03 12:30:00 +0000

    a

--- main.tf
+++ main.tf
@@ -24,6 +24,28 @@ module "ec2_instance" {
   }
 }
 
+module "ec2_instance" {
+  source                      = "./modules/ec2_module"
+  #ami_id                      = "ami-0e8d228ad90af673b"
+  #instance_type               = "t2.micro"
+  #key_name                    = "ec2-ansible"
+  instance_name               = "slb002"
+  subnet_id                   = "subnet-0082cb4d471785dd3"
+  associate_public_ip_address = true
+  root_volume_size            = 20
+  #root_volume_type            = "gp2"
+  #availability_zone           = "eu-west-2a"
+  #create_additional_volume     = true
+  #additional_volume_name      = "slb001-volume001"
+  #additional_volume_size      = 20
+  #additional_volume_type      = "gp2"
+  #additional_volume_device_name = "/dev/xdfh"
+  tags              = { 
+    Env       = "dev"
+    Team      = "infra"
+    Terraform = "yes" 
+  }
+}
 
 
 ###########################
@@ -65,20 +87,5 @@ module "vpc" {
   private_subnets = {
     "eu-west-2a" = "10.0.101.0/24"
     "eu-west-2b" = "10.0.102.0/24"
-  }
-}
-
-
-module "vpc-prod" {
-  source         = "./modules/vpc_module"
-  name           = "prod-pvc"
-  vpc_cidr       = "10.0.0.0/16"
-  public_subnets = {
-    "eu-west-2a" = "10.0.3.0/24"
-    "eu-west-2b" = "10.0.4.0/24"
-  }
-  private_subnets = {
-    "eu-west-2a" = "10.0.103.0/24"
-    "eu-west-2b" = "10.0.104.0/24"
   }
 }
