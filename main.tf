d5eef6e73631d957ee549ed6cdaa434fc2cd9f0c
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-12-03 10:55:14 +0000

    a

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@
 ###     S3 BUCKETS      ###
 ###########################
 module "s3_bucket" {
-  source = "./s3_module"
+  source = "./modules/s3_module"
 
   bucket_name             = "fabiom-20241203"
   enable_versioning       = true
