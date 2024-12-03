bc781fa610cdc0fc09f266af094b712f80e2ee5e
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-12-03 12:33:01 +0000

    a

--- main.tf
+++ main.tf
@@ -22,21 +22,22 @@ module "ec2_instance" {
     Team      = "infra"
     Terraform = "yes" 
   }
+}
 
-
+module "ec2_instance_2" {
   source                      = "./modules/ec2_module"
   #ami_id                      = "ami-0e8d228ad90af673b"
   #instance_type               = "t2.micro"
   #key_name                    = "ec2-ansible"
-  instance_name               = "slb002"
+  instance_name               = "slb001"
   subnet_id                   = "subnet-0082cb4d471785dd3"
   associate_public_ip_address = true
   root_volume_size            = 20
   #root_volume_type            = "gp2"
   #availability_zone           = "eu-west-2a"
-  #create_additional_volume     = true
-  #additional_volume_name      = "slb001-volume001"
-  #additional_volume_size      = 20
+  create_additional_volume     = true
+  additional_volume_name      = "slb001-volume001"
+  additional_volume_size      = 20
   #additional_volume_type      = "gp2"
   #additional_volume_device_name = "/dev/xdfh"
   tags              = { 
@@ -45,8 +46,6 @@ module "ec2_instance" {
     Terraform = "yes" 
   }
 }
-
-
 
 
 ###########################
