8824496e4786668a4603042233f28fb1ed706cae
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-12-03 12:18:04 +0000

    a

--- modules/vpc_module/outputs.tf
+++ modules/vpc_module/outputs.tf
@@ -3,15 +3,15 @@ output "vpc_id" {
   value       = aws_vpc.this.id
 }
 
-output "public_subnet_ids" {
-  description = "IDs of public subnets"
-  value       = aws_subnet.public[*].id
-}
+# output "public_subnet_ids" {
+#   description = "IDs of public subnets"
+#   value       = aws_subnet.public[*].id
+# }
 
-output "private_subnet_ids" {
-  description = "IDs of private subnets"
-  value       = aws_subnet.private[*].id
-}
+# output "private_subnet_ids" {
+#   description = "IDs of private subnets"
+#   value       = aws_subnet.private[*].id
+# }
 
 output "internet_gateway_id" {
   description = "Internet Gateway ID"
