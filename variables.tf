72c1baa99e8271a680831a68cbcb266d03cf81f3
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-12-04 13:42:41 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
-variable "AWS_ACCESS_KEY_ID" {
-  type        = string
+variable "aws_access_key_id" {
   description = "AWS Access Key ID"
+  type        = string
 }
 
-variable "AWS_SECRET_ACCESS_KEY" {
-  type        = string
+variable "aws_secret_access_key" {
   description = "AWS Secret Access Key"
+  type        = string
 }
 
 variable "AWS_REGION" {
