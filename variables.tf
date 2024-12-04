668124564b7235165cdf30c11b125460c47b276c
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-12-04 13:16:45 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
-variable "AWS_ACCESS_KEY" {}
+variable "AWS_ACCESS_KEY_ID" {}
 
-variable "AWS_SECRET_ACCESS" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
 
 variable "AWS_REGION" {
 default = "eu-west-2"
