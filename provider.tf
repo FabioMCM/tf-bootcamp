c20186b648568724f9ab13f68b50bd914874734e
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-12-04 15:11:34 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  version = "~> 4.0"  # specify an appropriate version
+  version = "~> 5.78.0"  # specify an appropriate version
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
   region     = var.AWS_REGION
