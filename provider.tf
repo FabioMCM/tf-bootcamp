37f87a9b16a0a892df7b3db02654655f637cebe6
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-12-04 12:19:37 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_ACCESS
   region     = "eu-west-2"
 }
