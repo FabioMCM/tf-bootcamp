18d763190466daa2d1d5a88e21c25b2f6e30bc80
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-12-05 14:22:23 +0000

    a

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "dev-tf-statefm"    # Replace with your bucket name
-    key    = "/dev/terraform.tfstate"  # Path where the state file will be stored
+    key    = "dev/terraform.tfstate"  # Path where the state file will be stored
     region = "eu-west-2"                   # Replace with the region of your bucket
     encrypt = true                         # Encrypt the state file in S3 (recommended)
   }
