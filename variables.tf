18a730ea26e37e17d418dd45383f2f8c0f78d741
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-12-06 11:32:54 +0000

    review code

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
-variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_ACCESS_KEY_ID" {
+  sensitive = true
+}
 
-variable "AWS_SECRET_ACCESS_KEY" {}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  sensitive = true
+}
 
 variable "AWS_REGION" {
   default = "eu-west-2"
