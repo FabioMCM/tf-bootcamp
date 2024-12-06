fe827a52dced30c13cf893a648889a9ab24be99f
Fábio Monteiro <50633301+DovakPT@users.noreply.github.com>
2024-12-06 10:57:35 +0000

    additional_volume - slb001

--- ec2_instances.tf
+++ ec2_instances.tf
@@ -12,9 +12,9 @@ module "ec2_instance_slb001" {
   root_volume_size            = 20
   #root_volume_type            = "gp2"
   #availability_zone           = "eu-west-2a"
-  #create_additional_volume     = true
-  #additional_volume_name      = "slb001-volume001"
-  #additional_volume_size      = 20
+  create_additional_volume     = true
+  additional_volume_name      = "slb001-volume001"
+  additional_volume_size      = 20
   #additional_volume_type      = "gp2"
   #additional_volume_device_name = "/dev/xdfh"
   tags              = { 
